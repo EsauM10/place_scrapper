1e96b88ce69e37fcf845732fcca7a2928e877cd8
EsauM10 <esau2096@gmail.com>
2023-03-27 21:28:12 -0300

    build: add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="place_scrapper",
+    version="0.0.0",
+    description='Scrapping package to retrieve data from Google Places.',
+    long_description='Scrapping package to retrieve data from Google Places.',
+    keywords=['google places', 'maps', 'scrapping', 'selenium'],
+    url='https://github.com/EsauM10/place_scrapper',
+    author='Esaú Mascarenhas',
+    install_requires = [
+        'selenium'
+    ],
+    packages=find_packages(),
+    zip_safe=False
+)
